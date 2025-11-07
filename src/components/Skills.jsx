833e5e291788607edbdd8d54afd35a9ef1b5fd06
Mahta <mahta.ir@gmail.com>
2025-11-07 10:04:35 +0100

    skillsEdit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,82 +14,53 @@ import Photoshop from '../assets/photoshop.png';
 import illus from '../assets/illustrator.png';
 import vue from '../assets/vueJs.png';
 
-
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full min-h-screen bg-[#0a192f] text-gray-300 pb-20 sm:pb-0'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-[90%] h-full'>
-          <div>
-              <p className='text-4xl font-bold inline border-b-4 border-[#FF9533] text-[#FF9533]'>Skills</p>
-              <p className='py-4'>These are the technologies I've worked with</p>
-          </div>
+        <div>
+          <p className='text-4xl font-bold inline border-b-4 border-[#FF9533] text-[#FF9533]'>Skills</p>
+          <p className='py-4'>These are the technologies I've worked with</p>
+        </div>
 
-          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
-                  <p className='my-4'>HTML</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
-                  <p className='my-4'>CSS</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon" />
-                  <p className='my-4'>JAVASCRIPT</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
-                  <p className='my-4'>REACT</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={GitHub} alt="HTML icon" />
-                  <p className='my-4'>GITHUB</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={WordPress} alt="HTML icon" />
-                  <p className='my-4'>WordPress</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind icon" />
-                  <p className='my-4'>Tailwind</p>
-              </div>
-              {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={vue} alt="Tailwind icon" />
-                  <p className='my-4'>Vue Js</p>
-              </div> */}
-              {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={illus} alt="Tailwind icon" />
-                  <p className='my-4'>Illustarator</p>
-              </div> */}
-              {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Photoshop} alt="Tailwind icon" />
-                  <p className='my-4'>Photoshop</p>
-              </div> */}
-              
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Figma} alt="Figma icon" />
-                  <p className='my-4'>Figma</p>
-              </div>
-              {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Figma} alt="Figma icon" />
-                  <p className='my-4'>Photoshop</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Figma} alt="Figma icon" />
-                  <p className='my-4'>Figma</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Figma} alt="Figma icon" />
-                  <p className='my-4'>Figma</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Figma} alt="Figma icon" />
-                  <p className='my-4'>Figma</p>
-              </div> */}
-              
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+            <p className='my-4'>HTML</p>
           </div>
-          
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
+            <p className='my-4'>CSS</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript icon" />
+            <p className='my-4'>JavaScript</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={ReactImg} alt="React icon" />
+            <p className='my-4'>React</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={GitHub} alt="GitHub icon" />
+            <p className='my-4'>GitHub</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={WordPress} alt="WordPress icon" />
+            <p className='my-4'>WordPress</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind icon" />
+            <p className='my-4'>Tailwind</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Figma} alt="Figma icon" />
+            <p className='my-4'>Figma</p>
+          </div>
+        </div>
+
+        {/* Spacer for mobile */}
+        <div className="block sm:hidden h-16"></div>
       </div>
     </div>
   );
